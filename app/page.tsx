1af090b3d95e6132bf9fc6e8c0dc0e0fa7a8d78f
Hogwarts26 <dieu0823@gmail.com>
2026-02-04 22:15:04 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1096,7 +1096,7 @@ export default function HogwartsApp() {
 
               const studentName = selectedName; 
               const masterData = studentMasterData[studentName];
-              const totalMinutes = 8000;
+              const totalMinutes = 79999;
 
               let levelCount = 1;
               if (totalMinutes >= 12000) levelCount = 4;
