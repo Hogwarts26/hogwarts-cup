97c239cf2946eda43eb261e1b449ce5e46fd3779
Hogwarts26 <dieu0823@gmail.com>
2026-02-15 23:41:44 +0900

    Update page.tsx

--- app/planner /page.tsx
+++ app/planner /page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../lib/supabase';
 import Link from 'next/link';
 
 export default function PlannerPage() {
